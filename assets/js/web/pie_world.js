63b7f346a66bd89de31c9739e71ce01afedfa1a8
tamimi-dev <60556811+tamimi-dev@users.noreply.github.com>
2020-03-09 00:56:12 -0400

    Update pie_world.js

--- assets/js/web/pie_world.js
+++ assets/js/web/pie_world.js
@@ -1,30 +1,24 @@
-      $.getJSON("https://spreadsheets.google.com/feeds/list/1XEhGO2sCwpCrrx4O3clmomZxe9d2RIShWQoRYEzOpaI/oxcft20/public/values?alt=json", data => {
+      $.getJSON("https://spreadsheets.google.com/feeds/list/12Lyd3k0PRpe-Ie_LaeJaEPm3FwF3iz1gizAOSprn7l0/o935awr/public/values?alt=json", data => {
   var labels = [];
    var seri = [];
   data.feed.entry.forEach(e => {
-    labels.push(e['gsx$label']['$t']);
-
-    seri.push(Number(e['gsx$aaa']['$t']));
-
+    labels.push(e['gsx$piewlabel']['$t']);
+    seri.push(Number(e['gsx$piewcases']['$t']));
   });
-  
-      
+
       var options = {
          series: seri,
                 plotOptions: {
      pie: {
                     expandOnClick: true
-
      }
   },
           chart: {
           type: 'pie',
-                            foreColor: '#e0e0e0'
+            foreColor: '#e0e0e0'
         },
         labels: labels,
-        
   colors: ['#546E7A', '#E91E63'],
-
         responsive: [{
           options: {
             chart: {
@@ -35,12 +29,8 @@
             }
           }
         }]
-
         };
-
-        var chart = new ApexCharts(document.querySelector("#chart"), options);
+        var chart = new ApexCharts(document.querySelector("#chart_w"), options);
         chart.render();
-         
-
     });  
      
