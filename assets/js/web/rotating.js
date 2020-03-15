67525e20390405d94fa54dcb262387ef33fd5cac
tamimi-dev <60556811+tamimi-dev@users.noreply.github.com>
2020-03-15 00:39:28 -0400

    Update rotating.js

--- assets/js/web/rotating.js
+++ assets/js/web/rotating.js
@@ -9,7 +9,9 @@ chart.geodata = am4geodata_worldLow;
 
 // Set projection
 chart.projection = new am4maps.projections.Projection();
-
+chart.panBehavior = "rotateLongLat";
+chart.deltaLatitude = -20;
+chart.padding(20, 20, 20, 20);
 
 chart.backgroundSeries.mapPolygons.template.polygon.fillOpacity = 1;
 chart.backgroundSeries.mapPolygons.template.polygon.fill = am4core.color("#303032");
