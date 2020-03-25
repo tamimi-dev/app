96273e3c1115e4f1da9076d277a536bc597b397e
tamimi-dev <60556811+tamimi-dev@users.noreply.github.com>
2020-03-25 01:51:43 -0400

    Update icotablejs.js

--- assets/js/web/icotablejs.js
+++ assets/js/web/icotablejs.js
@@ -1,3 +1,5 @@
+
+
 $.getJSON("https://spreadsheets.google.com/feeds/list/12Lyd3k0PRpe-Ie_LaeJaEPm3FwF3iz1gizAOSprn7l0/otu1nbu/public/values?alt=json", function(data) {
 
   var sheetData = data.feed.entry;
@@ -5,16 +7,37 @@ $.getJSON("https://spreadsheets.google.com/feeds/list/12Lyd3k0PRpe-Ie_LaeJaEPm3F
   var i;
   for (i = 0; i < sheetData.length; i++) {
 
-    var cntry = data.feed.entry[i]['gsx$icocntry']['$t'];
-    var cntry_case = data.feed.entry[i]['gsx$icontotcasepercntry']['$t'];
+
     var ico = data.feed.entry[i]['gsx$icon']['$t'];
-    var dth = data.feed.entry[i]['gsx$dthcntry']['$t'];
-    var chc = data.feed.entry[i]['gsx$chc']['$t'];
-    var cdt = data.feed.entry[i]['gsx$chd']['$t'];
 
-    document.getElementById('ico_cntry').innerHTML += ('<tr><td><svg class="avatar avatar--120">' + '<g class="avatar__box">' + '<image xlink:href="https://tamimi-dev.github.io/app/assets/icons/cntry/' + ico + '.svg"' + ' height="100%"' + 'width="100%"></image></g></svg></td><td>' + cntry + '</td><td>' + cntry_case + '</td><td>' + chc + '</td><td>' + dth + '</td><td>' + cdt + '</td></tr>');
+
+   /* document.getElementById('ico').innerHTML += ('<tr><td><svg class="avatar avatar--120">'+'<g class="avatar__box">'+'<image xlink:href="https://tamimi-dev.github.io/app/assets/icons/cntry/'+ico+'.svg"'+'height="100%"'+'width="100%"></image></g></svg></td></tr>'); */
   }
-
 
 });
 
+$(document).ready(function() {
+$('#globe').dataTable( {
+"bServerSide":false,
+"bProcessing":true,
+"sAjaxDataProp": "feed.entry",
+"sAjaxSource": "https://spreadsheets.google.com/feeds/list/12Lyd3k0PRpe-Ie_LaeJaEPm3FwF3iz1gizAOSprn7l0/otu1nbu/public/values?alt=json",
+
+"columnDefs": [
+    { targets: 0,
+      render: function(ico) {
+        return '<svg class="avatar avatar--120"><g class="avatar__box"><image xlink:href="https://coronatrackers.com/assets/icons/cntry/'+ico+'.svg" height="100%" width="100%"></image></g></svg>'
+      }
+    } ],  
+
+"aoColumns": [
+
+{ "mDataProp": "gsx$icocntry.$t" },
+{ "mDataProp": "gsx$dthcntry.$t" },
+{ "mDataProp": "gsx$icontotcasepercntry.$t" },
+{ "mDataProp": "gsx$icontotcasepercntry.$t" },
+{ "mDataProp": "gsx$icontotcasepercntry.$t" },
+{ "mDataProp": "gsx$icontotcasepercntry.$t" },
+]
+});
+});
