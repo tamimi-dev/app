02224465b2e88c81106bc1e26497a6a87a90cc6f
tamimi-dev <60556811+tamimi-dev@users.noreply.github.com>
2020-03-28 13:44:24 -0400

    Create stacked_per_de.js

---
+++ assets/js/web/stacked_per_de.js
@@ -0,0 +1,90 @@
+$.getJSON("https://spreadsheets.google.com/feeds/list/12Lyd3k0PRpe-Ie_LaeJaEPm3FwF3iz1gizAOSprn7l0/ox8ow5n/public/values?alt=json", data => {
+  var labels1 = [];
+  var numbers = [];
+   var numbers2 = [];
+      var numbers3 = [];
+         var numbers4 = [];
+             var numbers5 = [];
+  data.feed.entry.forEach(e => {
+    labels1.push(e['gsx$dename']['$t']);
+    numbers.push(Number(e['gsx$decase']['$t']));
+	numbers2.push(Number(e['gsx$dedth']['$t']));
+  	numbers3.push(Number(e['gsx$deser']['$t']));
+
+  }); 
+
+var options = {
+          series: [{
+          name: 'Cases',
+          data: numbers
+        }, {
+          name: 'Deaths',
+          data: numbers2
+        }, {
+          name: 'Serious',
+          data: numbers3
+        }],
+	colors: ['#d67f29', '#fd304c', '#c107ff', '#ffc107', '#00a700'],
+
+          chart: {
+          type: 'bar',
+          height: 500,
+          stacked: true,
+           stackType: '100%',
+               foreColor: "#ccc"
+              
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true,
+            
+          },
+        },
+        stroke: {
+          width: 1,
+          colors: ['#fff']
+        },
+        title: {
+          text: 'Germany\'s Cases'
+        },
+        xaxis: {
+          categories: labels1,
+
+          labels: {
+            formatter: function (val) {
+              return val 
+            }
+          }
+
+          
+        },
+         scales: {
+	    yAxes: [{
+
+                             logarithmic: true,
+                              	    ticks: {
+          beginAtZero: true,
+        },
+
+	    }]
+    },
+        tooltip: {
+         theme: "dark",
+         y: {
+            formatter: function (val) {
+              return val 
+            }
+          }
+        },
+        fill: {
+          opacity: 1
+        },
+        legend: {
+          position: 'bottom',
+          horizontalAlign: 'center',
+          offsetX: 90
+        }
+        };
+        var chart = new ApexCharts(document.querySelector("#chart_s"), options);
+        chart.render();
+      });
